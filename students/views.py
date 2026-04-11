e7f74996975d79450ced52e9fa3e0d93c8ac5987
ManharCK <manhargurukkal2@gmail.com>
2026-04-11 22:32:25 +0530

    updated

--- students/views.py
+++ students/views.py
@@ -141,6 +141,6 @@ def password_change_view(request):
             logout(request)
             return redirect("login")
 
-    # role based redirect back
+  
     template = "student_profile/password_change.html" if request.user.role == 'student' else "admin/password_change.html"
     return render(request, template, {})
